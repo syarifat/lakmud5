794ac0e7cf2ae187fe9eee146abbd7025969495e
syarifat <syarifahsanit@gmail.com>
2026-06-19 20:52:50 +0700

    style: update PDF header layout and typography across all report templates

--- resources/views/admin/laporan/pdf/cv_pemateri.blade.php
+++ resources/views/admin/laporan/pdf/cv_pemateri.blade.php
@@ -75,20 +75,20 @@
 </head>
 <body>
 
-    <table style="width: 100%; border-collapse: collapse; border-bottom: 4px double #000000; padding-bottom: 8px; margin-bottom: 20px;">
+    <table style="width: 100%; border-collapse: collapse; margin-bottom: 20px;">
         <tr>
-            <td style="width: 15%; text-align: left; vertical-align: middle; padding-bottom: 8px;">
-                <img src="{{ public_path('logo.png') }}" style="height: 85px; width: auto;">
+            <td style="width: 20%; text-align: left; vertical-align: middle; padding-bottom: 8px;">
+                <img src="{{ public_path('logo.png') }}" style="height: 110px; width: auto;">
             </td>
-            <td style="width: 85%; text-align: right; vertical-align: middle; padding-bottom: 8px; padding-right: 5px;">
-                <div style="font-family: 'Times New Roman', Times, serif; font-size: 14pt; color: #00A651; font-weight: bold; line-height: 1.25; text-transform: uppercase;">
+            <td style="width: 80%; text-align: right; vertical-align: middle; padding-bottom: 8px; padding-right: 5px;">
+                <div style="font-family: 'Times New Roman', Times, serif; font-size: 16pt; color: #00A651; font-weight: bold; line-height: 1.25; text-transform: uppercase;">
                     PANITIA PELAKSANA LATIHAN KADER MUDA V<br>
                     PIMPINAN ANAK CABANG<br>
                     IKATAN PELAJAR NAHDLATUL ULAMA<br>
                     IKATAN PELAJAR PUTRI NAHDLATUL ULAMA<br>
                     KECAMATAN KAUMAN
                 </div>
-                <div style="font-family: Arial, Helvetica, sans-serif; font-size: 9pt; color: #000000; font-weight: bold; line-height: 1.3; margin-top: 5px;">
+                <div style="font-family: Arial, Helvetica, sans-serif; font-size: 9pt; color: #000000; font-weight: bold; line-height: 1.35; margin-top: 5px;">
                     Jln. Sidoluhur Gg. II, Dsn. Bancaan, Ds. Mojosari, Kec. Kauman - Tulungagung<br>
                     0883011340460/089617377022<br>
                     pacipippkauman@gmail.com<br>
