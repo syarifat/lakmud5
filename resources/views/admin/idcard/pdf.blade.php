ff01a9e4ed274600704ed24c80b6eb3198eb1740
syarifat <syarifahsanit@gmail.com>
2026-06-20 12:58:28 +0700

    refactor: replace SVG stamp frame with CSS-based perforation layout and update participant info display format

--- resources/views/admin/idcard/pdf.blade.php
+++ resources/views/admin/idcard/pdf.blade.php
@@ -29,20 +29,35 @@
             height: 632pt;
             z-index: 1;
         }
-        .content-overlay {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 398pt;
-            height: 632pt;
-            z-index: 2;
-        }
-        .stamp-frame {
+        .stamp-bg {
             position: absolute;
             top: 195pt;
             left: 114pt;
             width: 170pt;
             height: 220pt;
+            background-color: #cca034;
+            border-radius: 8pt;
+            z-index: 2;
+        }
+        .dot {
+            position: absolute;
+            width: 14pt;
+            height: 14pt;
+            border-radius: 50%;
+            background-color: #F5F2E9;
+            z-index: 3;
+        }
+        .dot.left {
+            left: -7pt;
+        }
+        .dot.right {
+            right: -7pt;
+        }
+        .dot.top {
+            top: -7pt;
+        }
+        .dot.bottom {
+            bottom: -7pt;
         }
         .photo-container {
             position: absolute;
@@ -54,40 +69,12 @@
             border: 4px solid #fff;
             overflow: hidden;
             background-color: #fff;
-            z-index: 3;
+            z-index: 4;
         }
         .photo-img {
             width: 120pt;
             height: 120pt;
             display: block;
-        }
-        .text-nama {
-            position: absolute;
-            left: 145pt;
-            top: 512pt;
-            font-size: 14pt;
-            font-weight: bold;
-            color: #000;
-            z-index: 3;
-            width: 220pt;
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            line-height: 1;
-        }
-        .text-delegasi {
-            position: absolute;
-            left: 165pt;
-            top: 562pt;
-            font-size: 12.5pt;
-            font-weight: bold;
-            color: #000;
-            z-index: 3;
-            width: 200pt;
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            line-height: 1;
         }
     </style>
 </head>
@@ -97,66 +84,79 @@
         <!-- Background Template Image -->
         <img class="bg-template" src="{{ public_path('storage/file_template/template_idcard.PNG') }}">
 
-        <div class="content-overlay">
-            <!-- Stamp Frame SVG -->
-            <div class="stamp-frame">
-                <svg width="100%" height="100%" viewBox="0 0 170 220" xmlns="http://www.w3.org/2000/svg">
-                    <!-- Golden background -->
-                    <rect x="0" y="0" width="170" height="220" fill="#cca034" rx="8" ry="8" />
-                    <!-- Left cutouts (Beige fill color #F5F2E9 to match card background) -->
-                    <circle cx="0" cy="20" r="7" fill="#F5F2E9" />
-                    <circle cx="0" cy="45" r="7" fill="#F5F2E9" />
-                    <circle cx="0" cy="70" r="7" fill="#F5F2E9" />
-                    <circle cx="0" cy="95" r="7" fill="#F5F2E9" />
-                    <circle cx="0" cy="120" r="7" fill="#F5F2E9" />
-                    <circle cx="0" cy="145" r="7" fill="#F5F2E9" />
-                    <circle cx="0" cy="170" r="7" fill="#F5F2E9" />
-                    <circle cx="0" cy="195" r="7" fill="#F5F2E9" />
-                    <!-- Right cutouts -->
-                    <circle cx="170" cy="20" r="7" fill="#F5F2E9" />
-                    <circle cx="170" cy="45" r="7" fill="#F5F2E9" />
-                    <circle cx="170" cy="70" r="7" fill="#F5F2E9" />
-                    <circle cx="170" cy="95" r="7" fill="#F5F2E9" />
-                    <circle cx="170" cy="120" r="7" fill="#F5F2E9" />
-                    <circle cx="170" cy="145" r="7" fill="#F5F2E9" />
-                    <circle cx="170" cy="170" r="7" fill="#F5F2E9" />
-                    <circle cx="170" cy="195" r="7" fill="#F5F2E9" />
-                    <!-- Top cutouts -->
-                    <circle cx="20" cy="0" r="7" fill="#F5F2E9" />
-                    <circle cx="45" cy="0" r="7" fill="#F5F2E9" />
-                    <circle cx="70" cy="0" r="7" fill="#F5F2E9" />
-                    <circle cx="95" cy="0" r="7" fill="#F5F2E9" />
-                    <circle cx="120" cy="0" r="7" fill="#F5F2E9" />
-                    <circle cx="145" cy="0" r="7" fill="#F5F2E9" />
-                    <!-- Bottom cutouts -->
-                    <circle cx="20" cy="220" r="7" fill="#F5F2E9" />
-                    <circle cx="45" cy="220" r="7" fill="#F5F2E9" />
-                    <circle cx="70" cy="220" r="7" fill="#F5F2E9" />
-                    <circle cx="95" cy="220" r="7" fill="#F5F2E9" />
-                    <circle cx="120" cy="220" r="7" fill="#F5F2E9" />
-                    <circle cx="145" cy="220" r="7" fill="#F5F2E9" />
-                </svg>
-            </div>
+        <!-- Stamp Background with CSS Perforation -->
+        <div class="stamp-bg">
+            <!-- Left edge perforation -->
+            <div class="dot left" style="top: 20pt;"></div>
+            <div class="dot left" style="top: 45pt;"></div>
+            <div class="dot left" style="top: 70pt;"></div>
+            <div class="dot left" style="top: 95pt;"></div>
+            <div class="dot left" style="top: 120pt;"></div>
+            <div class="dot left" style="top: 145pt;"></div>
+            <div class="dot left" style="top: 170pt;"></div>
+            <div class="dot left" style="top: 195pt;"></div>
 
-            <!-- Participant's Photo inside the Circle -->
-            <div class="photo-container">
-                @if($peserta->pendaftaran && $peserta->pendaftaran->file_foto && file_exists(public_path('storage/' . $peserta->pendaftaran->file_foto)))
-                    <img class="photo-img" src="{{ public_path('storage/' . $peserta->pendaftaran->file_foto) }}">
-                @else
-                    <!-- Default Silhouette SVG if no photo upload exists -->
-                    <svg width="120pt" height="120pt" viewBox="0 0 100 100" fill="#fff" xmlns="http://www.w3.org/2000/svg" style="background-color: #E2E8F0;">
-                        <circle cx="50" cy="35" r="18" fill="#94A3B8" />
-                        <path d="M15 85 C15 65, 30 55, 50 55 C70 55, 85 65, 85 85 Z" fill="#94A3B8" />
-                    </svg>
-                @endif
-            </div>
+            <!-- Right edge perforation -->
+            <div class="dot right" style="top: 20pt;"></div>
+            <div class="dot right" style="top: 45pt;"></div>
+            <div class="dot right" style="top: 70pt;"></div>
+            <div class="dot right" style="top: 95pt;"></div>
+            <div class="dot right" style="top: 120pt;"></div>
+            <div class="dot right" style="top: 145pt;"></div>
+            <div class="dot right" style="top: 170pt;"></div>
+            <div class="dot right" style="top: 195pt;"></div>
 
-            <!-- Participant's Name overlay -->
-            <div class="text-nama">{{ $peserta->name }}</div>
+            <!-- Top edge perforation -->
+            <div class="dot top" style="left: 20pt;"></div>
+            <div class="dot top" style="left: 45pt;"></div>
+            <div class="dot top" style="left: 70pt;"></div>
+            <div class="dot top" style="left: 95pt;"></div>
+            <div class="dot top" style="left: 120pt;"></div>
+            <div class="dot top" style="left: 145pt;"></div>
 
-            <!-- Participant's Delegation overlay -->
-            <div class="text-delegasi">{{ $peserta->pendaftaran?->delegasi ?? 'PAC IPNU IPPNU KAUMAN' }}</div>
+            <!-- Bottom edge perforation -->
+            <div class="dot bottom" style="left: 20pt;"></div>
+            <div class="dot bottom" style="left: 45pt;"></div>
+            <div class="dot bottom" style="left: 70pt;"></div>
+            <div class="dot bottom" style="left: 95pt;"></div>
+            <div class="dot bottom" style="left: 120pt;"></div>
+            <div class="dot bottom" style="left: 145pt;"></div>
+
+            <!-- Default Silhouette Placeholder (renders only if no photo upload exists) -->
+            @if(!($peserta->pendaftaran && $peserta->pendaftaran->file_foto && file_exists(public_path('storage/' . $peserta->pendaftaran->file_foto))))
+                <!-- White circle head -->
+                <div style="position: absolute; top: 40pt; left: 50pt; width: 70pt; height: 70pt; border-radius: 50%; background-color: #fff; z-index: 3;"></div>
+                <!-- White arch body -->
+                <div style="position: absolute; bottom: 0; left: 20pt; width: 130pt; height: 75pt; border-radius: 65pt 65pt 0 0; background-color: #fff; z-index: 3;"></div>
+            @endif
         </div>
+
+        <!-- Participant's Photo inside the Circle -->
+        @if($peserta->pendaftaran && $peserta->pendaftaran->file_foto && file_exists(public_path('storage/' . $peserta->pendaftaran->file_foto)))
+            <div class="photo-container">
+                <img class="photo-img" src="{{ public_path('storage/' . $peserta->pendaftaran->file_foto) }}">
+            </div>
+        @endif
+
+        <!-- Row Nama -->
+        <table style="position: absolute; left: 45pt; top: 508pt; width: 310pt; border-collapse: collapse; z-index: 3; font-family: 'Helvetica Neue', Arial, sans-serif;">
+            <tr>
+                <td style="width: 80pt; font-size: 15pt; font-weight: bold; color: #0d2a4a; padding-bottom: 2px; vertical-align: bottom; line-height: 1;">Nama:</td>
+                <td style="font-size: 14.5pt; font-weight: bold; color: #000; border-bottom: 2.5px solid #0d2a4a; padding-bottom: 2px; vertical-align: bottom; text-align: left; line-height: 1;">
+                    {{ $peserta->name }}
+                </td>
+            </tr>
+        </table>
+
+        <!-- Row Delegasi -->
+        <table style="position: absolute; left: 45pt; top: 554pt; width: 310pt; border-collapse: collapse; z-index: 3; font-family: 'Helvetica Neue', Arial, sans-serif;">
+            <tr>
+                <td style="width: 80pt; font-size: 15pt; font-weight: bold; color: #0d2a4a; padding-bottom: 2px; vertical-align: bottom; line-height: 1;">Delegasi:</td>
+                <td style="font-size: 13.5pt; font-weight: bold; color: #000; border-bottom: 2.5px solid #0d2a4a; padding-bottom: 2px; vertical-align: bottom; text-align: left; line-height: 1;">
+                    {{ $peserta->pendaftaran?->delegasi ?? 'Belum Ditentukan' }}
+                </td>
+            </tr>
+        </table>
     </div>
 
 </body>
